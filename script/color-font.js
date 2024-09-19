2e657ae3979e599467e0c0db97bface55885c3b2
pritom-roy <pritomroy20062@gmail.com>
2024-09-19 08:55:33 +0600

    Primary Initialization Done

---
+++ script/color-font.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1E1E1E',
+                secondary: '#0874F2',
+            },
+            fontFamily: {
+                outfit: ["Outfit", 'sans-serif'],
+            },
+        },
+    },
+}
