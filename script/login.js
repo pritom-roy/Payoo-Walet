99e0cd5612a55226d59a1ea0ac809b41402048d9
pritom-roy <pritomroy20062@gmail.com>
2024-09-19 16:36:00 +0600

    login form done

---
+++ script/login.js
@@ -0,0 +1,14 @@
+document.getElementById('button-login')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+
+        if (phoneNumber === '1234' && pinNumber === '1234') {
+            console.log('you are logged in');
+            window.location.href = '/home.html';
+        } else {
+            alert('Wrong phone number or pin.');
+        }
+    });
